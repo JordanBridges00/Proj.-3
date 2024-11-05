b853b9329197354922043071a3b5bd0f06b04619
colbypapadakis <152203319+colbypapadakis@users.noreply.github.com>
2024-11-04 18:17:19 -0600

    Update SentimentClassifier.h

--- SentimentClassifier.h
+++ SentimentClassifier.h
@@ -1,1 +1,14 @@
+#include <unordered_map>
+#include "DSString.h"
+
+class SentimentClassifier {
+private:
+    std::unordered_map<DSString, int> positiveWords;
+    std::unordered_map<DSString, int> negativeWords;
+
+public:
+    void train(const std::string& trainingFile);
+    int predict(const DSString& tweet);
+    double evaluate(const std::string& testingFile, const std::string& groundTruthFile);
+};
 
