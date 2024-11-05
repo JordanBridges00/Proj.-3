f2a2e2e30802c18e00bd47698211d1a2d2ee876e
colbypapadakis <152203319+colbypapadakis@users.noreply.github.com>
2024-11-04 18:22:19 -0600

    Update SentimentClassifier.h

--- SentimentClassifier.h
+++ SentimentClassifier.h
@@ -1,4 +1,8 @@
+#ifndef SENTIMENTCLASSIFIER_H
+#define SENTIMENTCLASSIFIER_H
+
 #include <unordered_map>
+#include <string>
 #include "DSString.h"
 
 class SentimentClassifier {
@@ -7,8 +11,18 @@ private:
     std::unordered_map<DSString, int> negativeWords;
 
 public:
+    // Trains the classifier using the provided training dataset file
     void train(const std::string& trainingFile);
+
+    // Predicts the sentiment of a given tweet
+    // Returns 1 for positive and 0 for negative
     int predict(const DSString& tweet);
+
+    // Evaluates the classifier's accuracy against a testing dataset
+    // Returns the accuracy as a double (percentage of correct predictions)
     double evaluate(const std::string& testingFile, const std::string& groundTruthFile);
 };
+
+#endif // SENTIMENTCLASSIFIER_H
+
 
